e272d6b9b0cb41b5b954d37071d2f1e66cb9d46b
Eliud-ko <eliudkonkara@gmail.com>
2025-05-18 12:32:29 +0300

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -2,9 +2,9 @@
 const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
-global.ownername = "xZiyy";
-global.owner = "6283159451487";
-global.botname = "clairity bot";
+global.ownername = "Eliud";
+global.owner = "254725100848";
+global.botname = "Xtime_Base_Pco";
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
